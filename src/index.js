7602e1a6aaace07d7dee8cad8719032273a0df41
lpb1602 <lponsb98@gmail.com>
2025-05-15 17:07:36 +0200

    Added the API AI

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-function generateCompliment(event) {
-  event.preventDefault();
+function displayCompliment(response) {
   new Typewriter("#compliment", {
-    strings: "The output will appear here",
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
+function generateCompliment(event) {
+  event.preventDefault();
+  let instructionsInput = document.querySelector("#user-instructions");
+  let key = "275d2e1f3c946100ob7479e0d25c3atd";
+  let prompt = `Generate a compliment based on the user mood ${instructionsInput.value} `;
+  let context =
+    "You are a friendly AI assistant. Your mission is to generate a short compliment based on the users mood. Truncate text if needed. Sign your compliment with '- SheCodes AI' in a <strong> element after a <br> element";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${key}`;
+
+  axios.get(apiURL).then(displayCompliment);
 }
 
 let complimentFormElement = document.querySelector("#compliment-generator");
